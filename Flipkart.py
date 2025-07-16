f57bbff63620907cab655cfa5c9d30845a8b971f
GANAPATI <ganapathigouda379@gmail.com>
2025-07-16 14:08:49 +0530

    streamlit_interface

--- Flipkart.py
+++ Flipkart.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Add browser-like headers
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
